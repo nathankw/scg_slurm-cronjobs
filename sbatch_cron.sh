8484fbf86933b824d74d4d7e35565ab80d402bff
Nathaniel Watson <nathankw@stanford.edu>
2018-03-26 17:19:02 -0700

    first commit

---
+++ sbatch_cron.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#SBATCH --account=gbsc
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10M
+#SBATCH -J clean_dx_projects
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=nathankw@stanford.edu
+#SBATCH --output=/srv/gsfs0/projects/gbsc/workspace/nathankw/sbatch_jobs/Log_DxCleanApps/dx_clean_projects.txt
+#SBATCH --open-mode=append
+#SBATCH --requeue
+#SBATCH --time=10
+
+module load gbsc/scgpm_seqresults_dnanexus
+
+scgpm_clean_raw_data.py -o org-snyder_encode -d 1
+scgpm_clean_raw_data.py -o org-cescg -d 1
+
+sbatch --begin=noon $0
